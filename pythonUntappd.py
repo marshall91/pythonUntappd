1eab9d0a6a722e37031aa8ec6f7ff2ea1178ac6c
mackenzie.marshall <mackenzie.marshall@hootsuite.com>
2015-03-23 17:39:16 -0700

    Updated api endpoint to https

--- pythonUntappd.py
+++ pythonUntappd.py
@@ -18,7 +18,7 @@ class api:
         client_secret = Untappd API Client Secret
     """
     def __init__(self, client_id, client_secret):
-        self.url = 'http://api.untappd.com/v4/'
+        self.url = 'https://api.untappd.com/v4/'
         self.client_id = client_id
         self.client_secret = client_secret
         self.auth = None
